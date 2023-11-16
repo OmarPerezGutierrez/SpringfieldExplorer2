f42e2553285d67df874b35978002ea100d76d7f5
SAREDGP\kitog <a20200196@utem.edu.mx>
2023-11-15 21:03:44 -0600

    no message

--- Scripts/app.js
+++ Scripts/app.js
@@ -12,48 +12,40 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         window.location.href = 'home.html';
     }, 50000);
-});
 
-document.addEventListener('DOMContentLoaded', () => {
-    checkConnectionStatus();
+    // Establecer un flag para controlar si la notificación ya se mostró
+    let notificationShown = false;
 
     window.addEventListener('online', () => {
-        showNotification('La aplicación está en línea');
+        if (!notificationShown) {
+            showNotification('La aplicación está en línea', 'online-icon.png');
+            notificationShown = true;
+        }
     });
 
     window.addEventListener('offline', () => {
-        showNotification('La aplicación está fuera de línea');
+        showNotification('La aplicación está fuera de línea', '../Images/offline.png');
+        notificationShown = false; // Reiniciar el flag cuando cambia a estado "offline"
     });
+
+    checkConnectionStatus();
 });
 
 function checkConnectionStatus() {
     if (navigator.onLine) {
-        showNotification('La aplicación está en línea');
+        showNotification('La aplicación está en línea', 'online-icon.png');
     } else {
-        showNotification('La aplicación está fuera de línea');
+        showNotification('La aplicación está fuera de línea', '../Images/offline.png');
     }
 }
 
-function showNotification(message) {
+function showNotification(message, icon) {
     if ('Notification' in window) {
         Notification.requestPermission().then(permission => {
             if (permission === 'granted') {
                 new Notification('Estado de conexión', {
                     body: message,
-                    icon: 'Images/logo.png'
-                });
-            }
-        });
-    }
-}
-
-function showNotification(message) {
-    if ('Notification' in window) {
-        Notification.requestPermission().then(permission => {
-            if (permission === 'granted') {
-                new Notification('Estado de conexión', {
-                    body: message,
-                    icon: 'Images/logo.png'
+                    icon: `Images/${icon}`
                 });
             }
         });
@@ -62,7 +54,7 @@ function showNotification(message) {
 
 document.addEventListener('DOMContentLoaded', () => {
     const backgroundAudio = document.getElementById('backgroundAudio');
-    
+
     if (backgroundAudio) {
         document.addEventListener('click', () => {
             backgroundAudio.play().catch(error => {
